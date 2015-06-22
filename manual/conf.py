97b8023dd21fdc3e20192f94b3923367c741c93c
gossi <github@gos.si>
2015-06-22 20:45:22 +0200

    Updated docs

--- manual/conf.py
+++ manual/conf.py
@@ -228,7 +228,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'images/about.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
